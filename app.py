115ea9775d54ea7fe09f8cf7795b977c46d786e2
kishanga <85829332+kishanga@users.noreply.github.com>
2024-09-10 22:57:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import base64
 
 
 
-st.set_page_config(page_title="Chat with Your AI Feng Shui Master", page_icon="👩🏻‍🏫", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat with Your .....", page_icon="👩🏻‍🏫", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 #Context
 
@@ -17,7 +17,7 @@ openai.api_key = st.secrets.openai_key
 
 
 # URL of the image you want to display
-image_url = "https://github.com/yuanman88/yuanman88.github.io/blob/main/FS%20_Banner.png?raw=true"
+image_url = ""
 
 # Display the image in Streamlit using HTML and CSS
 st.markdown(f"""
@@ -35,7 +35,7 @@ st.markdown(f"""
 """, unsafe_allow_html=True)
 
 # URL of the GIF you want to display
-gif_url = "https://github.com/yuanman88/yuanman88.github.io/blob/main/AI_Fengshui.gif?raw=true"
+gif_url = ""
 
 # Fetch the GIF from the URL
 response = requests.get(gif_url)
@@ -66,12 +66,12 @@ st.markdown(f"""
 
 
 # Display centered text
-#st.markdown("<p style='text-align: center;'>Welcome to the AI Feng Shui Master!</p>", unsafe_allow_html=True)
+#st.markdown("<p style='text-align: center;'>Welcome to the AI!</p>", unsafe_allow_html=True)
 
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Harmonizing energy flow... Please wait while we balance the chi! This may take 1-2 minutes."):
+    with st.spinner(text="This may take 1-2 minutes."):
         
         # Rebuild the storage context
         storage_context = StorageContext.from_defaults(persist_dir="./data/index.vecstore")
@@ -85,8 +85,7 @@ def load_data():
         context_window=2048, 
         
         system_prompt="""
-       You are an AI feng shui master and you are in the process of redesigning your living space with Feng Shui principles to ensure a harmonious and balanced environment. You're particularly interested in how the placement of certain objects can influence the energy flow and enhance the luck of the owner. Specifically, you're seeking guidance on the direction in which the stove should face to promote positive energy and prosperity in your home. Additionally, you'd appreciate insights on the optimal placement of other objects, such as the aquarium and bed, to further align with Feng Shui principles. Your goal is to gather comprehensive recommendations that foster luck, prosperity, and well-being, ensuring that every aspect of your living space contributes positively to the overall Feng Shui
-       Give the correct recommendation.
+       You are an AI.
         """
         )           
         return index
@@ -96,10 +95,10 @@ chat_engine = index.as_chat_engine(chat_mode="openai", verbose=True)
 
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask Me Feng Shui Questions Relating to Living Space 😊"}
+        {"role": "assistant", "content": "Ask Me Questions 😊"}
     ]
 
-if prompt := st.chat_input("Ask Me Feng Shui Questions Relating to Living Space"):
+if prompt := st.chat_input("Ask Me questions related to care giving"):
     # Save the original user question to the chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.session_state.new_question = True
